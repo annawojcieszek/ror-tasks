5213b179441bbee53659936ee2786627ede066b0
Anna Wojcieszek <wojcieszek.ania@gmail.com>
2013-03-10 20:43:50 +0100

    New specs and methods.

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -1,42 +1,106 @@
 class TodoList
 
+  class Item
+    attr_accessor :name
+    attr_reader :completed
+
+    def initialize(name, completed = false)
+      @name = name
+      @completed = completed
+    end
+
+    def to_s
+      @name.to_s
+    end
+
+    def complete
+      @completed = true
+    end
+
+    def uncomplete
+      @completed = false
+    end
+
+  end
+
+
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
     if items.nil?
       raise IllegalArgument
     end
-    @items = items
+    @items = []
+    items.each{|item| @items << Item.new(item)}
   end
+
   def empty?
     @items.empty?
   end
+
   def size
-    if @items.size > 0
-      1
-    else
-      0
-    end
+    @items.size
   end
+
   def <<(item)
-    @items.push(item)
+    @items << Item.new(item)
   end
+
   def last
     @items.last
-  end  
+  end
+
   def complete(item)
-    return true
+    @items[item].complete
   end
+
+  def uncomplete(item)
+    @items[item].uncomplete
+  end
+
+  # ukonczono?
   def completed?(item)
-    if item == 1
-      true
-    elsif item == 0
-      false
-    end
+    @items[item].completed
   end
+
   def first
-    if @items.first == @items.last
-      return @items[0]
-    end
+    @items.first
   end
-  
+
+  def completed
+    @items.select {|item| item.completed == true}#.map(&:to_s)
+  end
+
+  def uncompleted
+    @items.select {|item| item.completed == false}.map(&:to_s)
+  end
+
+  def delete(item)
+    @items.delete_at(item)
+  end
+
+  def remove_completed
+    @items.delete_if {|item| item.completed == true}
+  end
+
+  def reverse
+    @items.reverse!
+  end
+
+  def toggle(item)
+    item = @items[item]
+    item.completed ? item.uncomplete : item.complete
+  end
+
+  def change_description(item, description)
+    @items[item].name = description
+  end
+
+  def sort_by_name
+    @items.sort_by! {|x| x.name}
+  end
+
+  def [](item)
+    @items[item]
+  end
+
 end
--- task-1/spec/todo_list.rb
+++ task-1/spec/todo_list.rb
@@ -55,23 +55,70 @@ describe TodoList do
       list.completed?(0).should be_true
     end
   end
-  
+
   context "with many items" do
-    let(:items) { [item_description] }
-    
-    it { should_not be_empty }
-    
-    it "should return of completed items" do
-      
+    let(:second_item_description)    { "Iron Marek's shirts" }
+    let(:third_item_description)     { "Annoy Marek" }
+    let(:items) { [item_description, second_item_description] }
+
+    it "should return completed items" do
+      list.complete(0)
+      list.complete(1)
+      list.completed.size.should == items.size
+      list.completed.map(&:to_s).should == items
     end
-    
-    it "should remove of an individual item" do
-      item.delete
-      
+
+    it "should return uncompleted items" do
+      list.uncompleted.should == items
     end
-    
+
+    it "should remove individual item" do
+      n = list.size
+      list.delete(0)
+      list.size.should == n-1
+      list.first.to_s.should == second_item_description
+    end
+
+    it "should remove all completed items" do
+      list.complete(0)
+      list.complete(1)
+      list.remove_completed
+      list.size.should == 0
+    end
+
+    it "should reverse order of all items" do
+      list.reverse
+      list.first.to_s.should == second_item_description
+      list.last.to_s.should == item_description
+    end
+
+    it "should toggle the state of an item" do
+      list.complete(1)
+      list.toggle(0)
+      list.toggle(1)
+      list.completed?(0).should be_true
+      list.completed?(1).should be_false
+    end
+
+    it "should set the state of the item to uncompleted" do
+      list.uncomplete(0)
+      list.completed?(0).should be_false
+    end
+
+    it "should change the description of an item" do
+      list.change_description(0, "ania")
+      list.first.to_s.should == "ania"
+    end
+
     it "should sort the items by name" do
-      items.should_receive(:order).and_return
+      list << third_item_description
+      list.sort_by_name
+      list.first.to_s.should == third_item_description
+      list.last.to_s.should == second_item_description
+      list[1].to_s.should == item_description
+
     end
+
+
   end
 end
