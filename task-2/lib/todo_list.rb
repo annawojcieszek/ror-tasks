96ece6f600007e8d536739d9f9d157d59d8fc85c
Anna Wojcieszek <annawojcieszek@github.com>
2013-03-27 21:25:58 +0100

    Stub test complited

--- task-2/lib/todo_list.rb
+++ task-2/lib/todo_list.rb
@@ -1,6 +1,26 @@
 class TodoList
+  
+  class Item
+    def initialize(title, description)
+      @title = title
+      @description = description
+    end
+  end
+
 
   # Initialize the TodoList with +items+ (empty by default).
-  def initialize(items=[])
+  def initialize(items=[], args)
+    if args[:db].nil?
+      raise IllegalArgument
+    end
+    @items_count = args[:db].items_count
+  end
+  
+  def empty?
+    @items_count.zero?
+  end
+  
+  def size
+    @items_count
   end
 end
